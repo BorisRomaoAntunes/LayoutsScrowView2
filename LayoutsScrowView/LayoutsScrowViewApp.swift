37d6d9af97f1ca32f2faaced085f77ec2223e618
Boris Romão Antunes <62909074+BorisRomaoAntunes@users.noreply.github.com>
2023-07-31 10:18:19 -0300

    Initial Commit

---
+++ LayoutsScrowView/LayoutsScrowViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutsScrowViewApp.swift
+//  LayoutsScrowView
+//
+//  Created by Boris R on 31/07/23.
+//
+
+import SwiftUI
+
+@main
+struct LayoutsScrowViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
